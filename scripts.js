5b1d98bfd6378efd7867c55377381b145cf66009
Soheib <idersoheib@gmail.com>
2024-12-13 16:32:54 +0100

    implement user added recipe fucntion, limited to showing in console

--- scripts.js
+++ scripts.js
@@ -19,9 +19,9 @@ const recipeObject = {
 
 // getting the elements from the page
 const recipeName = document.querySelector('.recipe_name')
-const recipeImg = document.querySelector('.recipe_img')
-const ingredientsList = document.querySelector('.ingredients_list')
-const cookingSteps = document.querySelector('.cooking_steps')
+const recipeImg = document.querySelector('.recipeImg')
+const ingredientsList = document.querySelector('.ingredientsList')
+const cookingSteps = document.querySelector('.cookingSteps')
 
 // linking the elements to the recipe's object
 
@@ -35,3 +35,71 @@ for (let element of recipeObject.ingredients) {
 }
 
 cookingSteps.innerText = recipeObject.description
+
+// user added recipe section
+let newRecipeObject = {
+    id: 0,
+    title: "",
+    picture_url:
+      "",
+    ingredients: [],
+    description: "",
+}
+
+const addRecipeButton = document.querySelector('.addRecipeButton')
+
+addRecipeButton.addEventListener('click', addRecipe)
+
+// adding a recipe function
+function addRecipe(event) {
+    const parent = event.target.parentNode
+    const newRecipeDiv = document.createElement('div');
+    newRecipeDiv.classList.add('NewRecipeContainer'); 
+    const HTMLString = `
+    <br>
+    <label for="newRecipeName">Name</label>
+    <input type="text" id="newRecipeName" placeholder="Give your recipe a name" />
+    <button class="addNameButton">Add</button>
+    `;
+    parent.insertAdjacentHTML('beforeend', HTMLString);
+    const addNewName = document.querySelector('.addNameButton')
+    addNewName.addEventListener('click', addName)
+    
+}
+
+function addName(event){
+    const parent = event.target.parentNode
+    const nameInput = document.querySelector('#newRecipeName');
+    newRecipeObject.title = nameInput.value 
+    const HTMLString = `
+    <br>
+    <label for="ingredient1">Add ingredient</label>
+    <input type="text" id="ingredient1" placeholder="ingredient name"/><input id="amount1" type="number" min="0" max="9999" placeholder="amount"/><label for="unit1">unit</label><select id="unitDropDown"><option value="gr" selected>grams</option><option value="dl">dl</option><option value="cup">cup</option><option value="units">units</option></select>
+    <button class="addIngredient">Add</button>
+    `
+    parent.insertAdjacentHTML('beforeend', HTMLString);
+    const addNewIngredient = document.querySelector('.addIngredient')
+    addNewIngredient.addEventListener('click', addIngredient)
+}
+
+function addIngredient(event) {
+    const ingredientNameInput = document.querySelector('#ingredient1');
+    const ingredientAmountInput = document.querySelector('#amount1');
+    const ingredientUnitInput = document.querySelector('#unitDropDown');
+
+    const newIngredient = {
+        NAME: ingredientNameInput.value,
+        AMOUNT: `${ingredientAmountInput.value} ${ingredientUnitInput.value}`
+    };
+
+    
+    newRecipeObject.ingredients.push(newIngredient);
+
+    // Clear inputs for the next ingredient
+    ingredientNameInput.value = '';
+    ingredientAmountInput.value = '';
+    ingredientUnitInput.value = 'gr'; 
+
+ 
+    console.log(newRecipeObject.ingredients);
+}
